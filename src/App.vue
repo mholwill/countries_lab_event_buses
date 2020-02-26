626327f20550a3d2001b291316cfc125c7a05f33
mholwill <m_holwill@hotmail.co.uk>
2020-02-26 15:26:30 +0000

    list of countries added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>COUNTRIES</h1>
+    <div>
+      <countries-list :countries='countries'></countries-list>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from './components/CountriesList.vue';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
   components: {
-    HelloWorld
+    "countries-list": CountriesList
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div>
+    <li>{{country.name}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list-item',
+  props: ['country']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
